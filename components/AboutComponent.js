b612546493cc7ec05bcd10dc30a79c8fa0dc11d6
Mike Munley <spex1990@gmail.com>
2021-06-25 14:14:10 -0500

    Using Redux

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import { ScrollView, Text, FlatList} from 'react-native';
-import {Card, ListItem} from "react-native-elements";
-import { PARTNERS } from "../shared/partners"
+import { ScrollView, Text, FlatList } from 'react-native';
+import { Card, ListItem } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+    return {
+        partners: state.partners
+    };
+};
 
 class About extends Component {
-    constructor(props) {  
-        super(props);
-        this.state = {
-            partners: PARTNERS
-        }
-    }
 
     static navigationOptions = {
         title: 'About Us'
@@ -30,8 +31,7 @@ class About extends Component {
                 <ListItem
                     title={item.name}
                     subtitle={item.description}
-                    leftAvatar={{ source: require('./images/bootstrap-logo.png'
-                    )}}
+                    leftAvatar={{source: {uri: baseUrl + item.image}}}
                 />
             )
         };
@@ -40,10 +40,10 @@ class About extends Component {
         <ScrollView>
             <Mission />
             <Card title="Community Partners">
-                <FlatList
-                    data={this.state.partners}
-                    renderItem={renderPartner}
-                    keyExtractor={item => item.id.toString()}
+            <FlatList 
+                        data={this.props.partners.partners}
+                        renderItem={renderPartner}
+                        keyExtractor={item => item.id.toString()}
                     />
             </Card>
         </ScrollView>
@@ -51,4 +51,4 @@ class About extends Component {
     }
 };
 
-export default About;
+export default connect(mapStateToProps)(About);
